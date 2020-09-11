6eb46d11ba83d2d4a8144d08c579f54254b15463
Marthijn van Deelen <marthijnvandeelen@gmail.com>
2020-09-11 15:22:10 +0200

    Added lavemaze level

---
+++ PogChampRollAlBall/Assets/Scripts/LavaPoolController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaPoolController : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other)
+    {
+        
+        if(other.collider.gameObject.name == "Player")
+        {
+            Vector3 spawnPoint = new Vector3(1.83f, 3f, 0.75f);
+            other.transform.position = spawnPoint;
+            other.rigidbody.velocity = new Vector3(0, 0, 0);
+        }
+    }
+
+}
