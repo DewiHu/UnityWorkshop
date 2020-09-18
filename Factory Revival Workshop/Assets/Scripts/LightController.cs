1d92e28fd089477c0cb4d023fdd81c5ba580df6b
Dewi Hu <dewihu1995@gmail.com>
2020-09-18 14:21:11 +0200

    Added Light Example Scene

---
+++ Factory Revival Workshop/Assets/Scripts/LightController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    public Light[] PointLight;
+    public GameObject lights;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            foreach(var light in lights.GetComponentsInChildren<Light>())
+            {              
+                light.enabled = !light.enabled;
+            }
+        }
+    }
+}
