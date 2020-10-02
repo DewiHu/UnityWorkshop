d812b575e537837045d61209ffcdab91eb253d5d
Frank van Kan <Frank.vanKan@student.hu.nl>
2020-10-02 14:08:51 +0200

    Added key and key script

---
+++ Terrain Workshop/Assets/Scripts/KeyPickup.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyPickup : MonoBehaviour
+{
+
+    public GameObject key = null;
+    public GameObject gate = null;
+    [HideInInspector]
+    public bool hasKey = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.Equals(key))
+        {
+            hasKey = true;
+            gate.SetActive(!hasKey);
+            key.SetActive(!hasKey);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Zombie"))
+        {
+            hasKey = false;
+            gate.SetActive(!hasKey);
+            key.SetActive(!hasKey);
+        }
+    }
+}
