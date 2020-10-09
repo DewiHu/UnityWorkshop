0084ffb49a15ea78c77c718983f560a8be5f0e18
Dewi Hu <dewihu1995@gmail.com>
2020-10-09 15:05:04 +0200

    fix

---
+++ Spookshop/Assets/Scripts/FlailPickUp.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlailPickUp : MonoBehaviour
+{
+    public GameObject flail = null;
+    public bool hasFlail = false;
+    private new Transform camera;
+
+    void Start()
+    {
+        camera = transform.GetChild(0);
+    }
+
+    void Update()
+    {
+        if (hasFlail)
+        {
+            flail.transform.position = camera.position + camera.right * 0.637f + camera.up * -1.057f + camera.forward * 1.592f;
+            flail.transform.rotation = new Quaternion(camera.rotation.x, camera.rotation.y, camera.rotation.z, camera.rotation.w);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Flail") && !hasFlail)
+        {
+            Debug.Log("Collided with flail");
+            hasFlail = true;
+            other.gameObject.transform.SetParent(transform.GetChild(0));
+        }
+    }
+}
