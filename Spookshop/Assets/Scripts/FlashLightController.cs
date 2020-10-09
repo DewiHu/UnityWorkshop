dbdf4f3082fcae7b901104e04e7f8b5c2a10a5f2
Marthijn van Deelen <marthijnvandeelen@gmail.com>
2020-10-09 12:02:03 +0200

    Added player first perspn controller and flashlight battery GUI

---
+++ Spookshop/Assets/Scripts/FlashLightController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlashLightController : MonoBehaviour
+{
+    public Text percentagetext;
+    public Light flashlight;
+    public float percentage = 15f;
+    public float timeBetweenChanges = 10;
+    private float lastChange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(percentagetext == null)
+        {
+            Debug.Log("REEEEEE");
+        }
+
+        lastChange = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time - lastChange >= timeBetweenChanges && percentage > 0)
+        {
+            percentage--;
+            lastChange = Time.time;
+        }
+
+
+        percentagetext.text = percentage + "%";
+
+        if(percentage <= 0)
+        {
+            flashlight.intensity = 0;
+        }
+    }
+}
